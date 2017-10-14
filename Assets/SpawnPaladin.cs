8aa108b6b57ecdf65b8f918858dafcd98a05598c
Roldan Melcon <roldan@thecosmicwhale.com>
2017-10-14 02:59:40 -0700

    add new scene, create spawnpalidan script with mouseclick functionality and test quad interactions

---
+++ Assets/SpawnPaladin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPaladin : MonoBehaviour {
+
+	void OnMouseDown () {
+		print("clicked");
+	}
+	void OnClick() {
+		print("clicked");
+	}
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+		Ray ray = Camera.
+		main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				Debug.Log ("Name = " + hit.collider.name);
+				Debug.Log ("Tag = " + hit.collider.tag);
+				Debug.Log ("Hit Point = " + hit.point);
+				Debug.Log ("Object position = " + hit.collider.gameObject.transform.position);
+				Debug.Log ("--------------");
+			}
+		}
+	}
+}
