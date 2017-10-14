1a4a4b914c76628351b6ad2f3f9a7c0be16f14d5
WitsBitz <roldan@thecosmicwhale.com>
2017-10-13 18:06:03 -0600

    add more assets, add behaviours for dragon and paladin interactionsm

---
+++ Assets/DragonBehaviour.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonBehaviour : MonoBehaviour {
+
+	[SerializeField] private Transform raycastPoint;
+	[SerializeField] private GameObject fireBreathFX;
+
+	[SerializeField] private float detectionRadius = 3;
+
+	[SerializeField] private float fireDuration = 1;
+	[SerializeField] private float fireCooldown = 1;
+
+	private PaladinBehaviour paladin;
+	private Animator anim;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	void Update () 
+	{
+		if(paladin == null)
+			SearchForPaladin();
+		
+		print(paladin == null);
+	}
+
+	void SearchForPaladin()
+	{
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+			if(hitColliders[i].transform.gameObject.name == "Paladin")
+			{
+				paladin = hitColliders[i].transform.GetComponent<PaladinBehaviour>();
+				StartCoroutine(BreatheFire(paladin));
+			}
+            i++;
+        }
+
+		// RaycastHit hit;
+
+        // if (Physics.Raycast(raycastPoint.position, raycastPoint.forward, out hit, 100.0f))
+		// 	print(hit.transform.name);
+        //     if(hit.transform.gameObject.name == "Paladin")
+		// 	{
+		// 		paladin = hit.transform.GetComponent<PaladinBehaviour>();
+		// 		StartCoroutine(BreatheFire(paladin));
+		// 	}
+	}
+
+	IEnumerator BreatheFire(PaladinBehaviour paladin)
+	{
+		transform.LookAt(paladin.transform.position);
+		anim.SetBool("Breathe Fire", true);
+		fireBreathFX.SetActive(true);
+		StartCoroutine(paladin.SetOnFire());
+		yield return new WaitForSeconds(fireDuration);
+		anim.SetBool("Breathe Fire", false);
+		fireBreathFX.SetActive(false);
+		yield return new WaitForSeconds(fireCooldown);
+
+		if(paladin.health > 0)	
+			StartCoroutine(BreatheFire(paladin));
+		else
+			paladin = null;
+	}
+
+	public IEnumerator TakeDamage()
+	{
+		yield return new WaitForSeconds(1);
+	}
+}
---
+++ Assets/PaladinBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinBehaviour : MonoBehaviour {
+
+	public int health;
+
+	[SerializeField] private Transform raycastPoint;
+
+	private Animator anim;
+	private DragonBehaviour dragon;
+
+	void Awake () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	void Update () 
+	{
+		if(dragon == null)
+			SearchForDragon();
+	}
+
+	public void SearchForDragon()
+	{
+
+	}
+
+	public IEnumerator SetOnFire()
+	{
+		health--;
+		if(health <= 0)
+			anim.SetBool("Dead", true);
+
+		yield return new WaitForSeconds(1);
+	}
+}
